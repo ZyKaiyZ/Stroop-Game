0160af2c5ec876dea58ad15f605ce880416464a0
ZyKaiyZ <yoyo8117431@gmail.com>
2023-03-20 01:17:00 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Stroop Game
+## 這是一個有關Stroop Effect的小遊戲，也可以稱此遊戲為Stroop Color and Word Test(SCWT)或Stroop Test
+### 遊戲介紹:
+1. 一開始會先需要輸入遊玩的時間也就是倒計時的時間。
+
+    <img src="./img/gameStart.png" style="width:300px">
+2. 之後便開始遊玩，此遊戲需要遊玩者點擊文字描述的顏色而非文字顯示的顏色。
+
+    <img src="./img/gamePlaying.png" style="width:300px">
+
+3. 遊戲結束上面應會顯示出三個數字分別代表
+    - 笑臉為答對題數。
+    - 哭臉為答錯題數。
+    - 勾則為已作答數。
+
+    <img src="./img/gameRestart.png" style="width:300px">
---
+++ stroop.js
@@ -0,0 +1,73 @@
+const vm = Vue.createApp({
+    delimiters: ['[[', ']]'],
+    data() {
+        return {
+            fontColor: '',
+            colorText:"藍色",
+            correct: 0,
+            fail: 0,
+            done: 0,
+            timer: 0
+        }
+    },
+    methods:{
+        red: function(){
+            this.checkAnswer("紅色")
+        },
+        blue: function(){
+            this.checkAnswer("藍色")
+        },
+        green: function(){
+            this.checkAnswer("綠色")        
+        },
+        black: function(){
+            this.checkAnswer("黑色")
+        },
+        yellow: function(){
+            this.checkAnswer("黃色")
+        },
+        showAlert(bottonText){
+            swal({
+                title: 'Stroop Game',
+                icon: "success",
+                content: {
+                    element: "input",
+                    attributes: {
+                      placeholder: "輸入計時秒數"
+                    }
+                },
+                button: {
+                  text: bottonText,
+                },
+                closeOnClickOutside: false,
+            }).then((value) => {
+                this.countdown(value);
+            });
+        },
+        checkAnswer(colorParam){
+            var colorArray = ["紅色","藍色","綠色","黑色","黃色"];
+            var r = Math.floor(Math.random() * 5);
+            this.colorText == colorParam ? this.correct++ : this.fail++;
+            this.colorText = colorArray[r];
+            this.done = this.correct + this.fail;
+            this.getRandomColor();
+        },
+        getRandomColor(){
+            var colorArray = ["red","blue","green","black","rgb(203, 203, 0)"];
+            var r = Math.floor(Math.random() * 5);
+            this.fontColor = colorArray[r];
+        },
+        countdown(seconds) {
+            var interval = setInterval(()=>{
+                this.timer = seconds;
+                if(--seconds < 0){
+                    clearInterval(interval);
+                    this.showAlert("R e s t a r t")
+                }
+            }, 1000);
+        }
+    },
+    mounted(){
+        this.showAlert("S t a r t")
+    }
+}).mount('#app')
